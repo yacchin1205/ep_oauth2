a73ae82c082e84de0ddc8a9013e2256707baa0ad
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2023-05-08 18:29:57 +0900

    Fix for JupyterHub based on https://github.com/ether/ep_openid_connect/blob/main/index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var request = require('request');
 
 // var settings = require('ep_etherpad-lite/node/utils/Settings').ep_oauth2;
 var passport = require('passport');
-var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var OAuth2Strategy = require('passport-oauth2').Strategy;
 
 // Environment Variables
 var authorizationURL = process.env['EP_OAUTH2_AUTHORIZATION_URL'] || settings.users.oauth2.authorizationURL;
@@ -28,17 +28,20 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
+const ep = (endpoint) => `/ep_oauth2/${endpoint}`;
+const endpointUrl = (endpoint) => new URL(ep(endpoint).substring(1), publicURL).toString();
 
 function setUsername(token, username) {
-  console.debug('oauth2.setUsername: getting authorid for token %s', token);
-  authorManager.getAuthor4Token(token, function(err, author) {
-    if (ERR(err)) {
-      console.debug('oauth2.setUsername: could not get authorid for token %s', token);
-    } else {
+  console.debug('oauth2.setUsername: getting authorid for token %s', token, username);
+  authorManager.getAuthor4Token(token)
+    .then((author) => {
       console.debug('oauth2.setUsername: have authorid %s, setting username to "%s"', author, username);
       authorManager.setAuthorName(author, username);
-    }
-  });
+    })
+    .catch((err) => {
+      console.debug('oauth2.setUsername: could not get authorid for token %s', token);
+      console.error(err);
+    });
   return;
 }
 
@@ -49,7 +52,7 @@ exports.expressConfigure = function(hook_name, context) {
     tokenURL: tokenURL,
     clientID: clientID,
     clientSecret: clientSecret,
-    callbackURL: publicURL + '/auth/callback',
+    callbackURL: endpointUrl('callback'),
     scope: scope,
     proxy: proxy,
     state: state
@@ -69,13 +72,12 @@ exports.expressConfigure = function(hook_name, context) {
         accessToken: accessToken,
         refreshToken: refreshToken
       };
-      authorManager.createAuthorIfNotExistsFor(data[idKey], data[usernameKey], function(err, authorId) {
-        if (err) {
-          return cb(err);
-        }
-        data.authorId = authorId;
-        return cb(null, data);
-      });
+      authorManager.createAuthorIfNotExistsFor(data[idKey], data[usernameKey])
+        .then((author) => {
+          data.authorId = author.authorID;
+          cb(null, data);
+        })
+        .catch((err) => cb(err));
     });
   }));
   var app = context.app;
@@ -86,26 +88,98 @@ exports.expressConfigure = function(hook_name, context) {
 exports.expressCreateServer = function (hook_name, context) {
   console.info('oauth2-expressCreateServer');
   var app = context.app;
-  app.get('/auth/callback', passport.authenticate('hbp', {
-    failureRedirect: '/auth/failure'
-  }), function(req, res) {
-    req.session.user = req.user;
-    res.redirect(req.session.afterAuthUrl);
+  app.get(
+    ep('callback'),
+    (req, res, next) => {
+      req.epSession = req.session;
+      next();
+    },
+    passport.authenticate('hbp', {
+      failureRedirect: ep('authfailure'),
+    }),
+    (req, res) => {
+      req.session.ep_oauth2 = req.epSession.ep_oauth2;
+      const oauth2Session = req.session.ep_oauth2 || {};
+      oauth2Session.user = req.user;
+      delete req.session.user;
+      res.redirect(303, oauth2Session.next || publicURL);
+      delete oauth2Session.next;
+    }
+  );
+  app.get(
+    ep('login'),
+    passport.authenticate('hbp', {
+      failureRedirect: ep('authfailure')
+    })
+  );
+  app.get(ep('logout'), (req, res, next) => {
+    console.debug(`Processing ${req.url}`);
+    req.session.destroy(() => res.redirect(303, publicURL));
   });
-  app.get('/auth/failure', function(req, res) {
-    res.send("<em>Authentication Failed</em>")
-  });
-  app.get('/auth/done', function(req, res) {
-    res.send("<em>Authentication Suceeded</em>");
+  app.get(ep('authfailure'), function(req, res) {
+    res.send("<em>Authentication Failed</em>");
   });
 }
 
 exports.authenticate = function(hook_name, context) {
-  if (context.req.url.indexOf('/auth/') === 0) return context.next();
   console.info('oauth2-authenticate from ->', context.req.url);
-  context.req.session.afterAuthUrl = context.req.url;
-  return passport.authenticate('hbp')(context.req, context.res, context.next);
+  const {req, res, users} = context;
+  const {ep_oauth2: {user} = {}} = req.session;
+  if (!user) {
+    delete req.session.ep_oauth2;
+    return;
+  }
+  // Successfully authenticated.
+  console.info('Successfully authenticated user with userinfo:', user);
+  req.session.user = user;
+  console.debug('User properties:', req.session.user);
+  return true;
 }
+
+exports.authnFailure = (hookName, {req, res}) => {
+  // Reference from: https://github.com/ether/ep_openid_connect/blob/main/index.js
+  // Normally the user is redirected to the login page which would then redirect the user back once
+  // authenticated. For non-GET requests, send a 401 instead because users can't be redirected back.
+  // Also send a 401 if an Authorization header is present to facilitate API error handling.
+  //
+  // 401 is the status that most closely matches the desired semantics. However, RFC7235 section
+  // 3.1 says, "The server generating a 401 response MUST send a WWW-Authenticate header field
+  // containing at least one challenge applicable to the target resource." Etherpad uses a token
+  // (signed session identifier) transmitted via cookie for authentication, but there is no
+  // standard authentication scheme name for that. So we use a non-standard name here.
+  //
+  // We could theoretically implement Bearer authorization (RFC6750), but it's unclear to me how
+  // to do this correctly and securely:
+  //   * The userinfo endpoint is meant for the OAuth client, not the resource server, so it
+  //     shouldn't be used to look up claims.
+  //   * In general, access tokens might be opaque (not JWTs) so we can't get claims by parsing
+  //     them.
+  //   * The token introspection endpoint should return scope and subject (I think?), but probably
+  //     not claims.
+  //   * If claims can't be used to convey access level, how is it conveyed? Scope? Resource
+  //     indicators (RFC8707)?
+  //   * How is intended audience checked? Or is introspection guaranteed to do that for us?
+  //   * Should tokens be limited to a particular pad?
+  //   * Bearer tokens are only meant to convey authorization; authentication is handled by the
+  //     authorization server. Should Bearer tokens be processed during the authorize hook?
+  //   * How should bearer authentication interact with authorization plugins?
+  //   * How should bearer authentication interact with plugins that add new endpoints?
+  //   * Would we have to implement our own OAuth server to issue access tokens?
+  res.header('WWW-Authenticate', 'Etherpad');
+  if (!['GET', 'HEAD'].includes(req.method) || req.headers.authorization) {
+    res.status(401).end();
+    return true;
+  }
+  if (req.session.ep_oauth2 == null) req.session.ep_oauth2 = {};
+  req.session.ep_oauth2.next = new URL(req.url.slice(1), publicURL).toString();
+  res.redirect(303, endpointUrl('login'));
+  return true;
+};
+
+exports.preAuthorize = (hookName, {req}) => {
+  if (req.path.startsWith(ep(''))) return true;
+  return;
+};
 
 exports.handleMessage = function(hook_name, context, cb) {
   console.debug("oauth2.handleMessage");
